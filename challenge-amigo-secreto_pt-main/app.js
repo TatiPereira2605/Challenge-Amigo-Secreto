2843259d722ad870ef851639136c320f077c0345
Tatiana Pereira <tatianagomespereira@gmail.com>
2025-02-25 17:45:50 -0300

    Criar função 'adicionarAmigo' e 'limparCampo'

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,17 @@
 let nomeDosAmigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == '') {
+        alert ('Por favor, insira um nome!')
+    } else {
+        nomeDosAmigos.push(nome);
+    console.log(nomeDosAmigos)}
+
+    limparCampo()
+}
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
