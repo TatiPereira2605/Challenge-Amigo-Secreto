6bbc8c363f728c9c6f5fc7c513fd1d7cd70c91a9
Tatiana Pereira <tatianagomespereira@gmail.com>
2025-02-26 15:38:04 -0300

    Criar função "atualizar lista"

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,17 +1,30 @@
-let nomeDosAmigos = [];
+let listaAmigos = [];
+let listaElementos = document.getElementById('listaAmigos');
 
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
 
     if (nome == '') {
-        alert ('Por favor, insira um nome!')
+        alert ('Por favor, insira um nome!');
+        return
     } else {
-        nomeDosAmigos.push(nome);
-    console.log(nomeDosAmigos)}
-
-    limparCampo()
+        listaAmigos.push(nome);
+        console.log(listaAmigos);  
+    } 
+    limparCampo();
+    atualizarLista();
 }
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function atualizarLista() {
+    listaElementos.innerHTML = ''
+    
+    listaAmigos.forEach(nome => {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = nome;
+        listaElementos.appendChild(itemLista)
+    })
 }
