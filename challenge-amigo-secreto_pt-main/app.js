307ad2ff1caebcb311466f896e61aa258e1d4a2c
Tatiana Pereira <tatianagomespereira@gmail.com>
2025-02-27 13:23:03 -0300

    Criar função "sortear amigo"

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,15 +1,13 @@
 let listaAmigos = [];
-let listaElementos = document.getElementById('listaAmigos');
 
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
 
     if (nome == '') {
         alert ('Por favor, insira um nome!');
-        return
+        return;
     } else {
-        listaAmigos.push(nome);
-        console.log(listaAmigos);  
+        listaAmigos.push(nome);  
     } 
     limparCampo();
     atualizarLista();
@@ -20,7 +18,8 @@ function limparCampo() {
 }
 
 function atualizarLista() {
-    listaElementos.innerHTML = ''
+    let listaElementos = document.getElementById('listaAmigos');
+    listaElementos.innerHTML = '';
     
     listaAmigos.forEach(nome => {
         let itemLista = document.createElement('li');
@@ -28,3 +27,15 @@ function atualizarLista() {
         listaElementos.appendChild(itemLista)
     })
 }
+
+function sortearAmigo() {
+    if (listaAmigos.length < 3) {
+        alert('Digite pelo menos 3 nomes para sortear!');
+        return null;
+    } 
+        let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        let amigoSorteado = listaAmigos[indiceAleatorio];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `Seu amigo secreto é ${amigoSorteado}`;
+        
+    }
